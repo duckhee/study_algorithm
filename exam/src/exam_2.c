7574d8300939ea5293352cf8e8b15c7a4e6820a5
duckheewon <fain9301@yahoo.com>
2018-01-30 18:11:14 +0900

    exam add

---
+++ exam/src/exam_2.c
@@ -0,0 +1,22 @@
+/*
+ * exam_2.c
+ *
+ *  Created on: 2018. 1. 30.
+ *      Author: CANDH
+ */
+
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void main()
+{
+	int num, i, data;
+	srand(time(NULL));
+	num = rand() % 10;
+	i = 1;
+
+}
---
+++ exam/src/examp_1.c
@@ -0,0 +1,44 @@
+/*
+ * exam_1.c
+ *
+ *  Created on: 2018. 1. 30.
+ *      Author: CANDH
+ */
+
+
+
+
+#include<stdio.h>
+
+void main()
+{
+
+	char key;
+	int data;
+
+
+	printf("10진수->16진수 변환 프로그램\n");
+	printf("10진수를 16진수로 바꾸려면, [A] 키를 누르고, \n");
+	printf("16진수를 10진수로 바꾸려면, [B] 키를 누르시오 \n");
+	printf("a나 b를 누르시오 : ");
+
+
+	scanf("%c", &key);
+	printf("변환할 숫자를 입력하시오 : ");
+
+	if(key == 'a' || 'A')
+	{
+		scanf("%d", &data);
+		printf("10진수 값 : %d --> 16진수 값 : %x\n", data, data);
+	}
+	else if(key == 'b' || 'B')
+	{
+		scanf("%x", &data);
+		printf("16진수 값  : %x --> 10진수 값 : %d", data, data);
+
+	}
+	else
+	{
+		printf("[A],[B] 둘 중에 하나만 눌러야 합니다.");
+	}
+}
